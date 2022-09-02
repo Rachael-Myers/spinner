9486b4265ca1b50b1db6d8657725a94188396795
Rachael-Myers <mantaloversmomma@gmail.com>
2022-09-02 11:54:19 -0700

    updating existing files and adding event listeners

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
+let wheelOfRunes = {};
+wheelOfRunes.listofanswers = ["Fehu.", "Ehwaz.", "Dagaz.", "Kenaz.", "Berkano.", "Ansuz.", "Algiz.", "Eiwaz.", "Thurisaz.", "Wunjo.", "Uruz.", "Tiwaz.", "Sowilo.", "Raido.", "Inguz.", "Perthro.", "Othila.", "Nauthiz.", "Mannaz.", "Laguz.", "Jera.", "Isa.", "Hagalaz.", "Gebo."];
+
+
+
 var wheel = document.querySelector('.wheel');
 var btn = document.querySelector('.button');
 var rotate = false;
